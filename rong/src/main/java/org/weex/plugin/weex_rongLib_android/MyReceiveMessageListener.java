f33d914302542c891926f2774378f4cceef00012
QYG_XXY-PC\QYG_XXY <617659171@qq.com>
2018-06-06 14:00:17 +0800

    test

--- rong/src/main/java/org/weex/plugin/weex_rongLib_android/MyReceiveMessageListener.java
+++ rong/src/main/java/org/weex/plugin/weex_rongLib_android/MyReceiveMessageListener.java
@@ -1,8 +1,17 @@
 package org.weex.plugin.weex_rongLib_android;
 
 
+import android.app.ActivityManager;
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.base.library.LogX;
 import com.base.library.MyWeexManager;
@@ -10,10 +19,13 @@ import com.google.gson.Gson;
 import com.taobao.weex.WXSDKInstance;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import io.rong.imlib.RongIMClient;
+import io.rong.imlib.model.Conversation;
 import io.rong.imlib.model.Message;
+import io.rong.push.notification.PushNotificationMessage;
 
 
 /**
@@ -37,7 +49,7 @@ public class MyReceiveMessageListener implements RongIMClient.OnReceiveMessageLi
      */
     @Override
     public boolean onReceived(final Message message, int left) {//开发者根据自己需求自行处理
-        LogX.e("MessageListener", "->" + new Gson().toJson(message));
+        Log.e("MessageListener", "rong--" + new Gson().toJson(message));
         Map<String, Object> params = new HashMap<>();
         params.put("message", message);
         params.put("left", left);
@@ -52,119 +64,152 @@ public class MyReceiveMessageListener implements RongIMClient.OnReceiveMessageLi
         return false;
     }
 
-//    private void showNotification(final Conversation.ConversationType conversationType, final String targentId, final String title, final String content, final String ticker) {
+    private void showNotification(final Conversation.ConversationType conversationType, final String targentId, final String title, final String content, final String ticker) {
 //        if (targentId.equals(Constant.USER_ID_TYPE.TASK_HALL)) return;
 //        showNotification(conversationType, targentId, title, content, ticker, null);
-//    }
-//
-//    private void showNotification(final Conversation.ConversationType conversationType, final String targentId, final String title, final String content, final String ticker, final String msgType) {
-//        if (AppUtils.isAppIsInBackground(context)) {//在后台
-//            RongApi.getConversationNotificationStatus(conversationType, targentId, new RongIMClient.ResultCallback<Conversation.ConversationNotificationStatus>() {
-//                @Override
-//                public void onSuccess(Conversation.ConversationNotificationStatus conversationNotificationStatus) {
-//                    switch (conversationNotificationStatus) {
-//                        case DO_NOT_DISTURB:
-//                            LogUtils.e("ReceiveMessageListener", "消息免打扰，不提示!");
-//                            break;
-//                        case NOTIFY:
-//                            LogUtils.e("YUY", "conversationType = " + conversationType + " targentId = " + targentId);
-//                            buildNotification(conversationType, targentId, title, content, ticker, msgType);
-//                            break;
-//                    }
-//                }
-//
-//                @Override
-//                public void onError(RongIMClient.ErrorCode errorCode) {
-//                    LogUtils.e("ReceiveMessageListener", "获取消息免打扰状态失败，不提示!");
-//                }
-//            });
-//        }
-//    }
-//
-//    private void buildNotification(Conversation.ConversationType conversationType, String targentId, String title, String content, String ticker, String msgType) {
-//        if (mNotificationManager == null)
-//            mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
-//
-//        Intent intent = new Intent(context, RongMsgType.RC_CONTACTNTF_MSG.equals(msgType) ? NewFriendActivity.class : RongChatActivity.class);
-//        switch (conversationType) {
-//            case GROUP:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.GROUP);
-//                break;
-//            case PRIVATE:
-//                if (targentId.equals("4")) {
-//                    intent = new Intent(context, BusinessHallActivity.class);
-//                }
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.PRIVATE);
-//                break;
-//            case APP_PUBLIC_SERVICE:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.APP_PUBLIC_SERVICE);
-//                break;
-//            case CHATROOM:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.CHATROOM);
-//                break;
-//            case CUSTOMER_SERVICE:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.CUSTOMER_SERVICE);
-//                break;
-//            case DISCUSSION:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.DISCUSSION);
-//                break;
-//            case NONE:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.NONE);
-//                break;
-//            case PUBLIC_SERVICE:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.PUBLIC_SERVICE);
-//                break;
-//            case PUSH_SERVICE:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.PUSH_SERVICE);
-//                break;
-//            case SYSTEM:
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.SYSTEM);
-//                break;
-//        }
-//        intent.putExtra(RongChatMsgFragment.KEY_TARGET, targentId);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-////        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-//        mBuilder.setContentTitle(title)//设置通知栏标题
-//                .setContentText(content) //设置通知栏显示内容
-//                .setContentIntent(pendingIntent) //设置通知栏点击意图
-//                //  .setNumber(number) //设置通知集合的数量
-//                .setTicker(ticker) //通知首次出现在通知栏，带上升动画效果的
-//                .setWhen(System.currentTimeMillis())//通知产生的时间，会在通知信息里显示，一般是系统获取到的时间
-//                .setPriority(Notification.PRIORITY_DEFAULT) //设置该通知优先级
-//                .setAutoCancel(true)//设置这个标志当用户单击面板就可以让通知将自动取消
-//                .setOngoing(false)//ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)
-//                .setDefaults(Notification.DEFAULT_ALL)//向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合
-//                //Notification.DEFAULT_ALL  Notification.DEFAULT_SOUND 添加声音 // requires VIBRATE permission
+    }
+
+    private void showNotification(final Conversation.ConversationType conversationType, final String targentId, final String title, final String content, final String ticker, final String msgType) {
+        if (isAppIsInBackground(context)) {//在后台
+
+        }
+    }
+
+
+
+    private void buildNotification(String title, String content) {
+        if (mNotificationManager == null)
+            mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "123");
+
+        Intent intent = new Intent(context.getApplicationInfo().packageName + ".main");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentTitle(title)//设置通知栏标题
+                .setContentText(content) //设置通知栏显示内容
+                .setContentIntent(pendingIntent) //设置通知栏点击意图
+                //.setNumber(number) //设置通知集合的数量
+                //.setTicker(ticker) //通知首次出现在通知栏，带上升动画效果的
+                //.setWhen(System.currentTimeMillis())//通知产生的时间，会在通知信息里显示，一般是系统获取到的时间
+                //.setPriority(Notification.PRIORITY_DEFAULT) //设置该通知优先级
+                .setAutoCancel(true)//设置这个标志当用户单击面板就可以让通知将自动取消
+                .setOngoing(false)//ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)
+                .setDefaults(Notification.DEFAULT_ALL);//向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合
+        //Notification.DEFAULT_ALL  Notification.DEFAULT_SOUND 添加声音 // requires VIBRATE permission
 //                .setSmallIcon(R.drawable.ic_luncher);//设置通知小ICON
-//        mNotificationManager.notify(NotificationID, mBuilder.build());
-//    }
-//
-//    public void MessageType(Message message) throws Exception {
-//        CDDQuanMsg cddQuanMsg = (CDDQuanMsg) message.getContent();
-//        if (!TextUtils.isEmpty(cddQuanMsg.getServiceType()) && Integer.parseInt(cddQuanMsg.getServiceType()) == 1) {//1朋友圈 2 图文  3 活动
-//            if (Integer.parseInt(cddQuanMsg.getOperateType()) == 0) {
-//                SharedPreferencesUtil.setOtherCacheData("Quanhead" + UserBean.getInstall(context).getId(), cddQuanMsg.getHeadImage());
-//                EventBus.getDefault().post(new Refresh());
-//            } else {
-//                String NumQuan = SharedPreferencesUtil.getOtherCacheData("Quan" + UserBean.getInstall(context).getId());
-//                SharedPreferencesUtil.setOtherCacheData("Quan" + UserBean.getInstall(context).getId(), TextUtils.isEmpty(NumQuan) ? String.valueOf(1) : String.valueOf((Integer.parseInt(NumQuan) + 1)));
-//                SharedPreferencesUtil.setOtherCacheData("QuanImg" + UserBean.getInstall(context).getId(), cddQuanMsg.getHeadImage());
-//                EventBus.getDefault().post(new Refresh());
-//            }
-//        } else if (!TextUtils.isEmpty(cddQuanMsg.getServiceType()) && Integer.parseInt(cddQuanMsg.getServiceType()) == 3) {
-//            String Numhuodong = SharedPreferencesUtil.getOtherCacheData("huodong" + UserBean.getInstall(context).getId());
-//            SharedPreferencesUtil.setOtherCacheData("huodong" + UserBean.getInstall(context).getId(), TextUtils.isEmpty(Numhuodong) ? String.valueOf(1) : String.valueOf((Integer.parseInt(Numhuodong) + 1)));
-//            SharedPreferencesUtil.setOtherCacheData("huodongImg" + UserBean.getInstall(context).getId(), cddQuanMsg.getHeadImage());
-//            EventBus.getDefault().post(new Refresh());
+        mNotificationManager.notify(NotificationID, mBuilder.build());
+    }
+
+
+//    private void startActivity(Context context, String pkgName, PushNotificationMessage message) {
+//        if (TextUtils.isEmpty(pkgName)) return;
+//        //判断app进程是否存活
+//        if (isAppAlive(context, pkgName)) {
+//            //如果存活的话，就直接启动DetailActivity，但要考虑一种情况，就是app的进程虽然仍然在
+//            //但Task栈已经空了，比如用户点击Back键退出应用，但进程还没有被系统回收，如果直接启动
+//            //DetailActivity,再按Back键就不会返回MainActivity了。所以在启动
+//            //DetailActivity前，要先启动MainActivity。
+//            Intent mainIntent = new Intent(pkgName + ".main");
+//            //将MainAtivity的launchMode设置成SingleTask, 或者在下面flag中加上Intent.FLAG_CLEAR_TOP,
+//            //如果Task栈中有MainActivity的实例，就会把它移到栈顶，把在它之上的Activity都清理出栈，
+//            //如果Task栈不存在MainActivity实例，则在栈顶创建
+//            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+////            Intent detailIntent = new Intent(context, DetailActivity.class);
+////            detailIntent.putExtra("name", "电饭锅");
+////            detailIntent.putExtra("price", "58元");
+////            detailIntent.putExtra("detail", "这是一个好锅, 这是app进程存在，直接启动Activity的");
+////            Intent[] intents = {mainIntent, detailIntent};
+////            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            mainIntent.putExtra(PUSH_RONG_INTENT_KEY, message);
+//            context.startActivity(mainIntent);
 //        } else {
-//            String NumTuwen = SharedPreferencesUtil.getOtherCacheData("tuwen" + UserBean.getInstall(context).getId());
-//            SharedPreferencesUtil.setOtherCacheData("tuwen" + UserBean.getInstall(context).getId(), TextUtils.isEmpty(NumTuwen) ? String.valueOf(1) : String.valueOf((Integer.parseInt(NumTuwen) + 1)));
-//            SharedPreferencesUtil.setOtherCacheData("tuwenImg" + UserBean.getInstall(context).getId(), cddQuanMsg.getHeadImage());
-//            EventBus.getDefault().post(new Refresh());
+//            //如果app进程已经被杀死，先重新启动app，将DetailActivity的启动参数传入Intent中，参数经过
+//            //SplashActivity传入MainActivity，此时app的初始化已经完成，在MainActivity中就可以根据传入             //参数跳转到DetailActivity中去了
+//            Log.i(TAG, "the app process is dead");
+//            Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(pkgName);
+//            launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+//            launchIntent.putExtra(PUSH_RONG_INTENT_KEY, message);
+//            context.startActivity(launchIntent);
 //        }
 //    }
+
+//    private void startActivity(Context context, String id, Conversation.ConversationType conversationType) {
+//        //判断app进程是否存活
+////        if(isAppAlive(context, "com.chewuwuyou.app")){
+////            //如果存活的话，就直接启动DetailActivity，但要考虑一种情况，就是app的进程虽然仍然在
+////            //但Task栈已经空了，比如用户点击Back键退出应用，但进程还没有被系统回收，如果直接启动
+////            //DetailActivity,再按Back键就不会返回MainActivity了。所以在启动
+////            //DetailActivity前，要先启动MainActivity。
+////            Intent mainIntent = new Intent(context, MainActivity.class);
+////            //将MainAtivity的launchMode设置成SingleTask, 或者在下面flag中加上Intent.FLAG_CLEAR_TOP,
+////            //如果Task栈中有MainActivity的实例，就会把它移到栈顶，把在它之上的Activity都清理出栈，
+////            //如果Task栈不存在MainActivity实例，则在栈顶创建
+////            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+////            Intent detailIntent = new Intent(context, DetailActivity.class);
+////            detailIntent.putExtra("name", "电饭锅");
+////            detailIntent.putExtra("price", "58元");
+////            detailIntent.putExtra("detail", "这是一个好锅, 这是app进程存在，直接启动Activity的");
+////            Intent[] intents = {mainIntent, detailIntent};
+////            context.startActivities(intents);
+////        }else {
+//        //如果app进程已经被杀死，先重新启动app，将DetailActivity的启动参数传入Intent中，参数经过
+//        //SplashActivity传入MainActivity，此时app的初始化已经完成，在MainActivity中就可以根据传入             //参数跳转到DetailActivity中去了
+//        Log.i("NotificationReceiver", "the app process is dead");
+//        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage("com.chewuwuyou.app");
+//        launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+//        Bundle args = new Bundle();
+//        args.putString(RongChatMsgFragment.KEY_TARGET, id);
+//        args.putSerializable(RongChatMsgFragment.KEY_TYPE, conversationType);
+//        launchIntent.putExtra(RE_TAG, args);
+//        context.startActivity(launchIntent);
+////        }
+//    }
+
+
+    public static boolean isAppAlive(Context context, String packageName) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> processInfos = activityManager.getRunningAppProcesses();
+        for (int i = 0; i < processInfos.size(); i++) {
+            if (processInfos.get(i).processName.equals(packageName)) {
+                Log.i("NotificationR",
+                        String.format("the %s is running, isAppAlive return true", packageName));
+                return true;
+            }
+        }
+        Log.i("NotificationR",
+                String.format("the %s is not running, isAppAlive return false", packageName));
+        return false;
+    }
+
+    /**
+     * 判断程序是否在前台运行,返回true表示在后台
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isAppIsInBackground(Context context) {
+        boolean isInBackground = true;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT_WATCH) {
+            List<ActivityManager.RunningAppProcessInfo> runningProcesses = am.getRunningAppProcesses();
+            for (ActivityManager.RunningAppProcessInfo processInfo : runningProcesses) {
+                if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {//前台程序
+                    for (String activeProcess : processInfo.pkgList) {
+                        if (activeProcess.equals(context.getPackageName())) {//说明在前台
+                            isInBackground = false;
+                        }
+                    }
+                }
+            }
+        } else {
+            List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
+            ComponentName componentInfo = taskInfo.get(0).topActivity;
+            if (componentInfo.getPackageName().equals(context.getPackageName())) {//说明在前台
+                isInBackground = false;
+            }
+        }
+        return isInBackground;
+    }
 }
 
 
--- rong/src/main/java/org/weex/plugin/weex_rongLib_android/MyReceivePushMseListener.java
+++ rong/src/main/java/org/weex/plugin/weex_rongLib_android/MyReceivePushMseListener.java
@@ -1,9 +1,14 @@
 package org.weex.plugin.weex_rongLib_android;
 
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.gson.Gson;
+
+import java.util.List;
 
 import io.rong.push.notification.PushMessageReceiver;
 import io.rong.push.notification.PushNotificationMessage;
@@ -12,68 +17,56 @@ import io.rong.push.notification.PushNotificationMessage;
  * Created by xxy on 2016/9/13 0013.
  */
 public class MyReceivePushMseListener extends PushMessageReceiver {
+    private static final String TAG = "RongPushListener";
 //    public static final String RE_TAG = "re_tag";
 //    public static final String RE_ID_TAG = "re_id_tag";
 
     @Override
     public boolean onNotificationMessageArrived(Context context, PushNotificationMessage message) {
-        Log.e("-push-", "--" + new Gson().toJson(message));
+        Log.e("rong", "--" + new Gson().toJson(message));
 //        if (message.getTargetId().equals(Constant.USER_ID_TYPE.TASK_HALL)) return true;
-        return true;
+        return false;
     }
 
     @Override
     public boolean onNotificationMessageClicked(Context context, PushNotificationMessage message) {
-        Log.e("-push-", "-onNotificationMessageClicked-" + new Gson().toJson(message));
-        //        Intent intent = new Intent(context, RongChatActivity.class);
-//        Conversation.ConversationType conversationType = null;
-//        switch (message.getConversationType()) {
-//            case GROUP:
-//                conversationType = Conversation.ConversationType.GROUP;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.GROUP);
-//                break;
-//            case PRIVATE:
-//                conversationType = Conversation.ConversationType.PRIVATE;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.PRIVATE);
-//                break;
-//            case APP_PUBLIC_SERVICE:
-//                conversationType = Conversation.ConversationType.APP_PUBLIC_SERVICE;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.APP_PUBLIC_SERVICE);
-//                break;
-//            case CHATROOM:
-//                conversationType = Conversation.ConversationType.CHATROOM;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.CHATROOM);
-//                break;
-//            case CUSTOMER_SERVICE:
-//                conversationType = Conversation.ConversationType.CUSTOMER_SERVICE;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.CUSTOMER_SERVICE);
-//                break;
-//            case DISCUSSION:
-//                conversationType = Conversation.ConversationType.DISCUSSION;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.DISCUSSION);
-//                break;
-//            case NONE:
-//                conversationType = Conversation.ConversationType.NONE;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.NONE);
-//                break;
-//            case PUBLIC_SERVICE:
-//                conversationType = Conversation.ConversationType.PUBLIC_SERVICE;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.PUBLIC_SERVICE);
-//                break;
-//            case PUSH_SERVICE:
-//                conversationType = Conversation.ConversationType.PUSH_SERVICE;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.PUSH_SERVICE);
-//                break;
-//            case SYSTEM:
-//                conversationType = Conversation.ConversationType.SYSTEM;
-//                intent.putExtra(RongChatMsgFragment.KEY_TYPE, Conversation.ConversationType.SYSTEM);
-//                break;
-//        }
-//        intent.putExtra(RongChatMsgFragment.KEY_TARGET, message.getTargetId());
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-////        context.startActivity(intent);
-//        startActivity(context, message.getTargetId(), conversationType);
+//        Log.e("-push-", "-onNotificationMessageClicked-" + new Gson().toJson(message));
+        startActivity(context, context.getApplicationInfo().packageName, message);
         return true;
+    }
+
+    public static final String PUSH_RONG_INTENT_KEY = "push_rong_intent_key";
+
+    private void startActivity(Context context, String pkgName, PushNotificationMessage message) {
+        if (TextUtils.isEmpty(pkgName)) return;
+        //判断app进程是否存活
+        if (isAppAlive(context, pkgName)) {
+            //如果存活的话，就直接启动DetailActivity，但要考虑一种情况，就是app的进程虽然仍然在
+            //但Task栈已经空了，比如用户点击Back键退出应用，但进程还没有被系统回收，如果直接启动
+            //DetailActivity,再按Back键就不会返回MainActivity了。所以在启动
+            //DetailActivity前，要先启动MainActivity。
+            Intent mainIntent = new Intent(pkgName + ".main");
+            //将MainAtivity的launchMode设置成SingleTask, 或者在下面flag中加上Intent.FLAG_CLEAR_TOP,
+            //如果Task栈中有MainActivity的实例，就会把它移到栈顶，把在它之上的Activity都清理出栈，
+            //如果Task栈不存在MainActivity实例，则在栈顶创建
+            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            Intent detailIntent = new Intent(context, DetailActivity.class);
+//            detailIntent.putExtra("name", "电饭锅");
+//            detailIntent.putExtra("price", "58元");
+//            detailIntent.putExtra("detail", "这是一个好锅, 这是app进程存在，直接启动Activity的");
+//            Intent[] intents = {mainIntent, detailIntent};
+//            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            mainIntent.putExtra(PUSH_RONG_INTENT_KEY, message);
+            context.startActivity(mainIntent);
+        } else {
+            //如果app进程已经被杀死，先重新启动app，将DetailActivity的启动参数传入Intent中，参数经过
+            //SplashActivity传入MainActivity，此时app的初始化已经完成，在MainActivity中就可以根据传入             //参数跳转到DetailActivity中去了
+            Log.i(TAG, "the app process is dead");
+            Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(pkgName);
+            launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            launchIntent.putExtra(PUSH_RONG_INTENT_KEY, message);
+            context.startActivity(launchIntent);
+        }
     }
 
 //    private void startActivity(Context context, String id, Conversation.ConversationType conversationType) {
@@ -107,22 +100,22 @@ public class MyReceivePushMseListener extends PushMessageReceiver {
 //        context.startActivity(launchIntent);
 ////        }
 //    }
-//
-//
-//    public static boolean isAppAlive(Context context, String packageName) {
-//        ActivityManager activityManager =
-//                (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-//        List<ActivityManager.RunningAppProcessInfo> processInfos
-//                = activityManager.getRunningAppProcesses();
-//        for (int i = 0; i < processInfos.size(); i++) {
-//            if (processInfos.get(i).processName.equals(packageName)) {
-//                Log.i("NotificationLaunch",
-//                        String.format("the %s is running, isAppAlive return true", packageName));
-//                return true;
-//            }
-//        }
-//        Log.i("NotificationLaunch",
-//                String.format("the %s is not running, isAppAlive return false", packageName));
-//        return false;
-//    }
+
+
+    public static boolean isAppAlive(Context context, String packageName) {
+        ActivityManager activityManager =
+                (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> processInfos
+                = activityManager.getRunningAppProcesses();
+        for (int i = 0; i < processInfos.size(); i++) {
+            if (processInfos.get(i).processName.equals(packageName)) {
+                Log.i("NotificationR",
+                        String.format("the %s is running, isAppAlive return true", packageName));
+                return true;
+            }
+        }
+        Log.i("NotificationR",
+                String.format("the %s is not running, isAppAlive return false", packageName));
+        return false;
+    }
 }
